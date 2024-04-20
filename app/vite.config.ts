5251ac2b2344b081e6528478718c01d78087f424
bal7hazar <bal7hazar@proton.me>
2024-04-20 10:43:41 +0200

    🔧 Add PWA manifest

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,11 +3,53 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Matchmaker",
+        short_name: "DMM",
+        description: "Match people to fight",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/assets/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/assets/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/assets/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/assets/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
